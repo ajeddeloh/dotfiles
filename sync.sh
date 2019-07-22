fd437f2b021bec255869dff3ba4937522b929c06
Andrew Jeddeloh <ajeddelo@redhat.com>
2019-07-22 16:10:39 -0700

    sync: be prettier

--- sync.sh
+++ sync.sh
@@ -5,9 +5,10 @@ files=$(cat files)
 for f in $files
 do
 	infs="${HOME}/${f}"
-	[ -L "$infs" ] && continue
+	[ -L "$infs" ] && echo "skipping $f" && continue
 	inrepo=$(readlink -f "home/$f")
 	mkdir -p $(dirname "$inrepo")
 	cp "$infs" "$inrepo"
 	ln -sf "$inrepo" "$infs"
+	echo "copy and linked $f"
 done
