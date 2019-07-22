f601c6f2fd90e52db4f11c9665a43e90feb7d0d2
Andrew Jeddeloh <ajeddelo@redhat.com>
2019-07-22 16:13:03 -0700

    sync: use readlink -m not -f
    readlink -f fails on nonexistent files. readlink -m does not

--- sync.sh
+++ sync.sh
@@ -6,7 +6,7 @@ for f in $files
 do
 	infs="${HOME}/${f}"
 	[ -L "$infs" ] && echo "skipping $f" && continue
-	inrepo=$(readlink -f "home/$f")
+	inrepo=$(readlink -m "home/$f")
 	mkdir -p $(dirname "$inrepo")
 	cp "$infs" "$inrepo"
 	ln -sf "$inrepo" "$infs"
